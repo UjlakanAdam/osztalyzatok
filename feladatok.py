a8ed2b5dcde977a858e23258c207acd875d4a433
UjlakanAdam <ujlakan.adam@gmail.com>
2025-10-21 15:10:02 +0200

    feladatok

--- feladatok.py
+++ feladatok.py
@@ -7,7 +7,7 @@ def feladat1(szamok):
     while(cv<len(szamok)):
         osszeg+=szamok[cv]
         cv+=1
-    print(f"Jegyek átlaga: {osszeg/len(szamok)}")
+    return osszeg, szamok
 
 def feladat2(szamok):
     cv=0
@@ -17,7 +17,7 @@ def feladat2(szamok):
         if lista==5:
             otos+=1
         cv+=1
-    print(f"Ötösök száma: {otos}")
+    return otos
 
 def feladat3(szamok):
     cv=0
@@ -27,7 +27,7 @@ def feladat3(szamok):
         if lista==1:
             egyes+=1
         cv+=1
-    print(f"Elégtelenek száma: {egyes}")
+    return egyes
 
 def feladat4(szamok):
     cv=0
@@ -38,43 +38,12 @@ def feladat4(szamok):
         if lista==1:
             diak+=1
         cv+=1
-    print(f"Ez a diák kapott elégtelent: {cv}")
+    return cv
 
 #def feladat5(szamok):
 
+
     
-
-import random
-
-def fel7():
-    
-    lo = []
-    i = 0
-
-    while i < 17:
-
-        z = random.randint(1, 6)
-
-        if z == 6:
-            sz = 5
-
-        else:
-            sz = z
-
-        lo.append(sz)
-
-    return lo
-
-def fel6(templist2):
-    i = 0
-
-    while  i < len(templist2):
-
-        diak = templist2[i]
-
-        print(f"Diák {i+1} : diak")
-
-        return diak
 
 
     
--- main.py
+++ main.py
@@ -1,7 +1,18 @@
 #[5,2,3,4,4,5,5,5,1,2,2,3,4,5,5,5,4,3,3,]
 import feladatok
+import feladatok2
+
 lista = [5,2,3,4,4,5,5,5,1,2,2,3,4,5,5,5,4,3,3]
-feladatok.feladat1(lista)
-feladatok.feladat2(lista)
-feladatok.feladat3(lista)
-feladatok.feladat4(lista)
+
+
+osszeg, szamok=feladatok.feladat1(lista)
+print(f"Jegyek átlaga: {osszeg/len(szamok)}")
+
+otos=feladatok.feladat2(lista)
+print(f"Ötösök száma: {otos}")
+
+egyes=feladatok.feladat3(lista)
+print(f"Elégtelenek száma: {egyes}")
+
+cv=feladatok.feladat4(lista)
+print(f"Ez a diák kapott elégtelent: {cv}")
