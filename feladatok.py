38e7be769af9ffd345045d6937a07c3029e77294
UjlakanAdam <ujlakan.adam@gmail.com>
2025-10-21 14:32:53 +0200

    feladat2

--- feladatok.py
+++ feladatok.py
@@ -1,2 +1,20 @@
-def feladat1():
+def feladat1(szamok):
+    cv=0
+    osszeg=0
+    while(cv<len(szamok)):
+        osszeg+=szamok[cv]
+        cv+=1
+    print(f"Jegyek átlaga: {osszeg/len(szamok)}")
+
+def feladat2(szamok):
+    cv=0
+    otos=0
+    while(cv<len(szamok)):
+        lista=szamok[cv]
+        if lista==5:
+            otos+=1
+        cv+=1
+    print(f"Ötösök száma: {otos}")
+    
+
     
